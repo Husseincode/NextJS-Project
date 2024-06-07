965d415312a0f1cd4e1b96be1bee3d86d4d74a8a
Aminat <aminatabiodun57@gmail.com>
2024-06-06 13:08:58 -1200

    update

--- next-js/src/app/components/Header/header.tsx
+++ next-js/src/app/components/Header/header.tsx
@@ -3,6 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { SignIn, UserButton } from "@clerk/nextjs"
 import Link from 'next/link';
 import './header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTaxi } from '@fortawesome/free-solid-svg-icons/faTaxi';
+import { faDashboard, faHome, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 const header_styling = {
     height: '70px',
@@ -16,12 +19,12 @@ const header_styling = {
 export default function Header(){
     return (
         <header className='shadow-sm text-white' style={header_styling}>
-            <h3><img width={'50x'} height={'50px'} src='/k.png' alt='k'/>ings Rewards</h3>
-            <div className='d-flex text-dark gap-3 p-2'>
-                <Link className='lnk text-white' href={''}>Home</Link>
-                <Link className='lnk text-white' href={''}>About </Link>
-                <Link className='lnk text-white' href={''}>Ride </Link>
-                <Link className='lnk text-white' href={''}>Dashboard </Link>
+            <h3 className='mt-1'>Kings Rewards</h3>
+            <div className='d-flex links text-dark gap-4 p-2'>
+                <Link className='lnk text-white' href={''}>Home <FontAwesomeIcon icon={faHome}/></Link>
+                <Link className='lnk text-white' href={''}>About <FontAwesomeIcon icon={faInfoCircle}/></Link>
+                <Link className='lnk text-white' href={''}>Ride <FontAwesomeIcon icon={faTaxi}/></Link>
+                <Link className='lnk text-white' href={''}>Dashboard <FontAwesomeIcon icon={faDashboard}/></Link>
                 <UserButton/>
             </div>
             <div style={{position: 'absolute', top: '70px', right: '0px'}}>
--- next-js/src/app/components/Hero/hero.tsx
+++ next-js/src/app/components/Hero/hero.tsx
@@ -1,10 +1,15 @@
 import './hero.css'
 import Image from 'next/image'
 
+let span_style = {
+    color: 'rgb(33, 37, 41)',
+    borderBottom: '3px solid black'
+}
+
 export default function Hero(){
     return (
         <div className="img text-white">
-            <h2>Every Journey is a Rewarding Adventure</h2>
+            <h2>Every Journey is a Rewarding <span style={span_style} className='text-dark'>Adventure!</span></h2>
             {/* <Image className='image' src={'/kingReward.png'} alt='logo' layout='fill'/> */}
         </div>
     )
