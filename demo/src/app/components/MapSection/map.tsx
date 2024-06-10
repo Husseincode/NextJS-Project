4665337b47aa6a11f6df9f44784a433b37bcddc9
Aminat <aminatabiodun57@gmail.com>
2024-06-09 19:30:43 -1200

    commit

---
+++ demo/src/app/components/MapSection/map.tsx
@@ -0,0 +1,65 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import './map-section.css';
+import React, { useContext, useEffect, useState } from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { SourceContext } from '../../../../context/SourceContext';
+import { DestinationContext } from '../../../../context/DestinationContext';
+
+const containerStyle = {
+  // width: '400px',
+  height: '700px'
+};
+
+const Map = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  });
+
+  const [ center, setCenter ] = useState<any>({
+    lat: -3.745,
+    lng: -38.523
+  });
+
+  const [map, setMap] = React.useState(null);
+  const {source, setSource} = useContext<any>(SourceContext);
+  const {destination, setDestination} = useContext<any>(DestinationContext);
+
+  const onLoad = React.useCallback(function callback(map: any) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map: any) {
+    setMap(null)
+  }, []);
+
+  useEffect(()=>{
+    if(source?.length !== [] && map){
+      setCenter({
+        lat: source.lat,
+        lng: source.lng
+      })
+      console.warn(center)
+    }
+  },[map, source])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={{mapId: 'e7eb2ff1f40a437a'}}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default Map;
---
+++ demo/src/app/components/recommended/dummyData.tsx
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 1,
+        name: 'UberX',
+        descriptio: 'Affordable every price',
+        price: 4.0
+    },
+    {
+        id: 2,
+        name: 'Uber Pet',
+        descriptio: 'Affordable every price',
+        price: 1.9
+    },
+    {
+        id: 3,
+        name: 'UberXL',
+        descriptio: 'Affordable every price',
+        price: 1.4
+    },
+    {
+        id: 4,
+        name: 'Comfort',
+        descriptio: 'Affordable every price',
+        price: 3.0
+    },
+    {
+        id: 5,
+        name: 'UberX',
+        descriptio: 'Affordable every price',
+        price: 5.0
+    },
+    {
+        id: 6,
+        name: 'UberX',
+        descriptio: 'Affordable every price',
+        price: 4.0
+    },
+]
